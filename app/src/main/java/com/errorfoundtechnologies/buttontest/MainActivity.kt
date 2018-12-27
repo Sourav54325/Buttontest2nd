77db528c2beb5ec95f99b5218c255b028aa90096
Ladoo <souravkumar54325@gmail.com>
2018-12-27 15:16:26 +0530

    Initial commit

---
+++ app/src/main/java/com/errorfoundtechnologies/buttontest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.errorfoundtechnologies.buttontest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bt1.setOnClickListener{
+            tv1.text="Hello...!!!"
+        }
+    }
+}
